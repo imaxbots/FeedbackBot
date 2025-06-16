96f1b3bdf7aa6c114d4bac5e1b39464e40abe73b
IMAX BOTS <imaxbots@gmail.com>
2025-06-16 15:06:41 +0530

    Update useless.py

--- useless.py
+++ useless.py
@@ -18,5 +18,5 @@ async def web_server():
 
 if __name__ == "__main__":
 
-    port = int(os.getenv("PORT", 8080)) 
+    port = int(os.getenv("PORT", 8000)) 
     web.run_app(web_server(), port=port)
