e9bbd3bd25c436ff386bb479d0e50ae4172adade
IMAX BOTS <imaxbots@gmail.com>
2025-06-16 17:13:23 +0530

    Update sudo.py

--- plugs/sudo.py
+++ plugs/sudo.py
@@ -17,7 +17,7 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-ADMINS = [int(x) for x in os.getenv("ADMINS", "").split(",") if x.strip().isdigit()]
+ADMINS = [int(x) for x in os.getenv("ADMINS", "7388366658").split(",") if x.strip().isdigit()]
 
 spam_block = {}
 LOGS_RATE_LIMIT_SECONDS = 30
